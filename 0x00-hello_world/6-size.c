e890221ef6cdace71291cf736594360ae70cfa1a
compassur2 <buntuheny@gmail.com>
2023-03-25 09:12:47 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)", sizeof(char));
+	printf("Size of an int: 4 byte(s)", sizeof(int));
+	printf("Size of a long int: 4 byte(s)", sizeof(long int));
+	printf("Size of a long long int: 8 byte(s)", sizeof(long long));
+	printf("Size of a float: 4 byte(s)", sizeof(float));
+	return (0);
+}
