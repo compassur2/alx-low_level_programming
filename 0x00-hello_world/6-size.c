746b9ab34a1a89225380c237eac0ac61ba754b67
compassur2 <buntuheny@gmail.com>
2023-03-25 14:18:59 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of a char:%d 1 byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int:%d 4 byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int:%d 4 byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int:%d 8 byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float:%d 4 byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char:%zu 1 byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int:%zu 4 byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int:%zu 4 byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int:%zu 8 byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float:%zu 4 byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
