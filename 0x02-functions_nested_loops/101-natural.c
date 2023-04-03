0a7a6ca030e47ec32dcb753e9286c2be4650bfc4
compassur2 <buntuheny@gmail.com>
2023-04-03 07:24:39 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
 * main - main function
+*
 * Return: always 0
 */
 
@@ -9,11 +11,11 @@ int main(void)
 
 int i, sum = 0;
 
-for (int i = 0 ; i < 1024 ;  i++)
+for (i = 0 ; i < 1024 ;  i++)
 {
 if ((i % 3 == 0 || i % 5 == 0))
 
-sum = sum + i;
+sum += i;
 }
 printf("%d\n", sum);
 return (0);
