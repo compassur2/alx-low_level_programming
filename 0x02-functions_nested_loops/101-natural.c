87bfa271504d8f4dbc5ec8a8b9714b9161eb4587
compassur2 <buntuheny@gmail.com>
2023-04-03 07:18:01 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,13 @@
 int main(void)
 {
 
-int sum;
+int i, sum = 0;
 
-sum = 0;
-
-for (int i = 1; i < 1024; i++)
+for (int i = 0 ; i < 1024 ;  i++)
 {
 if ((i % 3 == 0 || i % 5 == 0))
-{
+
 sum = sum + i;
-}
 }
 printf("%d\n", sum);
 return (0);
